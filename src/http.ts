617621d8732fabe52cf2170f51187529d0337438
SerkanÖ <serkan@handleforme.com>
2021-11-30 01:25:08 +0300

    remove commits

--- src/http.ts
+++ src/http.ts
@@ -6,26 +6,18 @@ const http = axios.create({
     baseURL: 'https://api.github.com'
 });
 
-
-
-// TODO header ı düzenleme yapılcak.
-// Add a request interceptor
 http.interceptors.request.use(function (config) {
-    // Do something before request is sent
     config.headers = {...config.headers, authorization:'Bearer '+ localStorage.getItem(accessToken)} ;
     return config;
 }, function (error) {
-    // Do something with request error
     return Promise.reject(error);
 });
 
-// Add a response interceptor
 http.interceptors.response.use(function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
     return response;
 }, function (error) {
     //TODO: show error
+    alert(JSON.stringify(error))
 });
 
 export default http;
